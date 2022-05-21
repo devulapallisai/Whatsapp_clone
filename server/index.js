1318c410555f3acfee7abad8c0dda7c8b7efee77
devulapallisai <ee20btech11013@iith.ac.in>
2022-05-21 17:58:21 +0530

    added

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.listen(PORT, () =>
+  console.log(`Your server is running on http://localhost:${PORT}`)
+);
